1bce6fda6c087231c2725bd1272bdfefdb6068c9
BaiMeow <3652290768@qq.com>
2026-05-08 14:32:49 +0800

    debug: multiple VPTERNLOGQ probe functions for encoding analysis

--- pow/vpternlog_probe.go
+++ pow/vpternlog_probe.go
@@ -3,3 +3,8 @@
 package pow
 
 func vpternlogProbe(a, b, c uint64) uint64
+func vpternlogProbe00(a, b, c uint64) uint64
+func vpternlogProbeFF(a, b, c uint64) uint64
+func vpternlogProbeF0(a, b, c uint64) uint64
+func vpternlogProbe96(a, b, c uint64) uint64
+func vpternlogProbeCC(a, b, c uint64) uint64
--- pow/vpternlog_probe.s
+++ pow/vpternlog_probe.s
@@ -2,17 +2,47 @@
 
 #include "textflag.h"
 
-// func vpternlogProbe(a, b, c uint64) uint64
-// Computes a ^ (~b & c) using VPTERNLOGQ with imm8=$0xA4.
-// Returns the result so Go can verify against scalar computation.
-TEXT ·vpternlogProbe(SB), NOSPLIT, $0-32
-    MOVQ a+0(FP), AX
-    MOVQ b+8(FP), BX
-    MOVQ c+16(FP), CX
-    VMOVQ AX, X0
-    VMOVQ BX, X1
-    VMOVQ CX, X2
-    VPTERNLOGQ $0xA4, Z2, Z1, Z0
-    VMOVQ X0, AX
-    MOVQ AX, ret+24(FP)
+// Probe 0: imm8=0x00 → should return 0 regardless of inputs
+TEXT ·vpternlogProbe00(SB), NOSPLIT, $0-32
+    MOVQ a+0(FP), AX; VMOVQ AX, X0
+    MOVQ b+8(FP), BX; VMOVQ BX, X1
+    MOVQ c+16(FP), CX; VMOVQ CX, X2
+    VPTERNLOGQ $0x00, Z2, Z1, Z0
+    VMOVQ X0, AX; MOVQ AX, ret+24(FP)
+    RET
+
+// Probe FF: imm8=0xFF → should return all 1s regardless of inputs
+TEXT ·vpternlogProbeFF(SB), NOSPLIT, $0-32
+    MOVQ a+0(FP), AX; VMOVQ AX, X0
+    MOVQ b+8(FP), BX; VMOVQ BX, X1
+    MOVQ c+16(FP), CX; VMOVQ CX, X2
+    VPTERNLOGQ $0xFF, Z2, Z1, Z0
+    VMOVQ X0, AX; MOVQ AX, ret+24(FP)
+    RET
+
+// Probe F0: imm8=0xF0 → should return c
+TEXT ·vpternlogProbeF0(SB), NOSPLIT, $0-32
+    MOVQ a+0(FP), AX; VMOVQ AX, X0
+    MOVQ b+8(FP), BX; VMOVQ BX, X1
+    MOVQ c+16(FP), CX; VMOVQ CX, X2
+    VPTERNLOGQ $0xF0, Z2, Z1, Z0
+    VMOVQ X0, AX; MOVQ AX, ret+24(FP)
+    RET
+
+// Probe 96: imm8=0x96 → f(a,b,c)=a^(b&c) → easy to verify
+TEXT ·vpternlogProbe96(SB), NOSPLIT, $0-32
+    MOVQ a+0(FP), AX; VMOVQ AX, X0
+    MOVQ b+8(FP), BX; VMOVQ BX, X1
+    MOVQ c+16(FP), CX; VMOVQ CX, X2
+    VPTERNLOGQ $0x96, Z2, Z1, Z0
+    VMOVQ X0, AX; MOVQ AX, ret+24(FP)
+    RET
+
+// Probe CC: imm8=0xCC → f(a,b,c)=a?b:c → if a then b else c
+TEXT ·vpternlogProbeCC(SB), NOSPLIT, $0-32
+    MOVQ a+0(FP), AX; VMOVQ AX, X0
+    MOVQ b+8(FP), BX; VMOVQ BX, X1
+    MOVQ c+16(FP), CX; VMOVQ CX, X2
+    VPTERNLOGQ $0xCC, Z2, Z1, Z0
+    VMOVQ X0, AX; MOVQ AX, ret+24(FP)
     RET
